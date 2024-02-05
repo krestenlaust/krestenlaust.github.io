ff2b33747632e8d6e9ee9af5afaf0025689830b2
Kresten Laust <krestenlaust@gmail.com>
2024-02-05 13:15:56 +0100

    Make FileGrid and Desktop have single-responsibility
    Signed-off-by: Kresten Laust <krestenlaust@gmail.com>

--- src/utility/desktop/fileGrid.ts
+++ src/utility/desktop/fileGrid.ts
@@ -1,9 +1,25 @@
 import {Filesystem} from "../filesystem";
-import * as lodash from "ext/lodash.js"
+import * as lodash from "ext/lodash";
 
 namespace Desktop {
     export class FileGrid {
         static readonly DOUBLE_CLICK_INTERVAL = 500; //Windows standard according to https://ux.stackexchange.com/a/40366
+
+        gridWidth: number; //GRID_WIDTH = 8;
+        gridHeight: number; //GRID_HEIGHT = 4;
+        folderPath: string; // "C:\\Users\\kress\\desktop"
+        containerElement: HTMLDivElement; // document.getElementById("desktop")
+
+        constructor(containerElement: HTMLDivElement, gridWidth: number, gridHeight: number, folderPath: string) {
+            this.gridWidth = gridWidth;
+            this.gridHeight = gridHeight;
+            this.folderPath = folderPath;
+            this.containerElement = containerElement;
+
+            containerElement.addEventListener("click", desktop_click);
+            containerElement.addEventListener("keydown", desktop_keydown);
+        }
+
         doubleClickPending: boolean = false;
 
         gridIcons = {
@@ -27,16 +43,8 @@ namespace Desktop {
             }
         };
 
-        gridWidth: number; //GRID_WIDTH = 8;
-        gridHeight: number; //GRID_HEIGHT = 4;
-        folderPath: string; // "C:\\Users\\kress\\desktop"
-        containerElement: HTMLDivElement; // document.getElementById("desktop")
-
-        constructor(gridWidth: number, gridHeight: number, folderPath: string, containerElement: HTMLDivElement) {
-            this.gridWidth = gridWidth;
-            this.gridHeight = gridHeight;
-            this.folderPath = folderPath;
-            this.containerElement = containerElement;
+        render(): void {
+            this.updateGrid();
         }
 
         updateGrid(){
@@ -95,9 +103,106 @@ namespace Desktop {
             keyboardSelectionIndex: 0
         };
 
-        dragging = class {
-            private draggingIndex = 0;
+        /*
+        function doubleclick_timer() {
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    resolve('resolved');
+                }, 500);
+            });
+        }*/
+
+        desktopClick(e) {
+            console.log("Desktop click");
+
+            let objectDiv: HTMLElement;
+            switch (e.target.tagName) {
+                case "IMG":
+                    objectDiv = e.target.parentElement;
+                    break;
+                case "P":
+                    objectDiv = e.target.parentElement;
+                    break;
+                case "DIV":
+                    objectDiv = e.target;
+                    break;
+                default:
+                    this.selectionProperties.selected_icons = [];
+            }
+
+            if (objectDiv.getAttribute("class") === "desktop-icon") {
+                let iconList = <HTMLCollectionOf<HTMLDivElement>>document.getElementsByClassName("desktop-icon");
+                for (let i = 0; i < iconList.length; i++) {
+
+                    if (iconList[i] === objectDiv && selectionProperties.selected_icons.indexOf(i) === -1) {
+                        if (e.ctrlKey || e.shiftKey) {
+                            this.selectionProperties.selected_icons.push(i);
+                        } else {
+                            this.selectionProperties.selected_icons = [i];
+                        }
+                    }
+                }
+            } else {
+                this.selectionProperties.selected_icons = [];
+            }
+            this.updateSelectedIcons();
         }
+
+        desktopKeydown(e) {
+            let iconList = <HTMLCollectionOf<HTMLDivElement>>document.getElementsByClassName("desktop-icon");
+
+            if (e.srcElement !== document.body) {
+                return;
+            }
+
+            let lastIndex = this.selectionProperties.keyboardSelectionIndex;
+            switch (e.code) {
+                case "ArrowUp":
+                    //selection = index - width
+                    this.selectionProperties.keyboardSelectionIndex -= this.gridWidth;
+                    if (this.selectionProperties.keyboardSelectionIndex < 0) {
+                        this.selectionProperties.keyboardSelectionIndex += this.gridWidth;
+                    }
+
+                    if (e.shiftKey) {
+                        //selectionProperties.selected_icons = [... range(lastIndex, dawdwa)]
+                    } else {
+                        this.selectionProperties.selected_icons = [this.selectionProperties.keyboardSelectionIndex];
+                    }
+
+                    break;
+                case "ArrowDown":
+                    //selection = index + width
+                    this.selectionProperties.keyboardSelectionIndex += this.gridWidth;
+                    if (this.selectionProperties.keyboardSelectionIndex >= this.gridWidth * this.gridHeight) {
+                        this.selectionProperties.keyboardSelectionIndex -= this.gridWidth;
+                    }
+
+                    break;
+                case "ArrowLeft":
+                    //selection = index - 1
+                    this.selectionProperties.keyboardSelectionIndex -= 1;
+                    if (this.selectionProperties.keyboardSelectionIndex < 0) {
+                        this.selectionProperties.keyboardSelectionIndex += 1;
+                    }
+
+                    break;
+                case "ArrowRight":
+                    //selection = index + 1
+                    this.selectionProperties.keyboardSelectionIndex += 1;
+                    if (this.selectionProperties.keyboardSelectionIndex >= this.gridWidth * this.gridHeight) {
+                        this.selectionProperties.keyboardSelectionIndex -= 1;
+                    }
+                    break;
+                case "Enter":
+                    for (let i = 0; i < this.selectionProperties.selected_icons.length; i++) {
+                        eval(iconList[this.selectionProperties.selected_icons[i]].dataset.launch);
+                    }
+                    break;
+            }
+        }
+
+        private draggingIndex = 0;
 
         desktopDrop(e) {
             e.preventDefault();
@@ -134,7 +239,26 @@ namespace Desktop {
             console.log(e);
         }
 
-        static desktopDragStart(e) {
+        desktopFiledrop(e) {
+            e.preventDefault();
+
+            if (!e.dataTransfer.items) {
+                return;
+            }
+
+            for (let i = 0; i < e.dataTransfer.items.length; i++) {
+                if (e.dataTransfer.items[i].kind === "file") {
+                    let file = e.dataTransfer.items[i].getAsFile();
+                    transferDataToFile(file);
+                }
+            }
+        }
+
+        desktopDragOver(e) {
+            e.preventDefault();
+        }
+
+        desktopDragStart(e) {
             let targetElement = e.target;
 
             if (!targetElement.classList.contains("space")) {
@@ -145,7 +269,7 @@ namespace Desktop {
                 }
             }
 
-            draggingIndex = parseInt(targetElement.dataset.index);
+            this.draggingIndex = parseInt(targetElement.dataset.index);
         }
     }
 }
